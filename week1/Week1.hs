c1eb3fee8c992f86890f00d8b2157cde6941308d
Michael Parker <michaelrbparker@protonmail.com>
2026-01-29 16:53:16 +0000

    first half of worksheet 1

---
+++ week1/Week1.hs
@@ -0,0 +1,27 @@
+
+timesTen :: Int -> Int
+timesTen num = num * 10
+
+sumThree :: Int -> Int -> Int -> Int
+sumThree a b c = a + b + c
+
+areaOfCircle :: Float -> Float
+areaOfCircle r = pi * r ^2
+
+volumeOfCylinder :: Float -> Float -> Float
+volumeOfCylinder h r = areaOfCircle ( r ) * h
+
+distance :: Float -> Float -> Float -> Float -> Float
+distance x1 y1 x2 y2 =  sqrt ((x1 - x2)^2 + (y1 - y2)^2)
+
+threeDifferent :: Int -> Int -> Int -> Bool
+threeDifferent a b c = (a /= b) && (b /= c)
+
+divisibleBy :: Int -> Int -> Bool
+divisibleBy a b = a `mod` b == 0
+
+isEven :: Int -> Bool
+isEven a = divisibleBy a 2
+
+averageThree :: Int -> Int -> Int -> Float
+averageThree a b c = fromIntegral (a + b + c) / 3
