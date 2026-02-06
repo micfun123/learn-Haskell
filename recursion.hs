33dcd36d1b411a1d5e0a7e5a7a2e7c4cb685e260
Michael Parker <michaelrbparker@protonmail.com>
2026-02-06 13:23:15 +0000

    update for lab

---
+++ recursion.hs
@@ -0,0 +1,3 @@
+fac n
+  n <= 1 = 1
+  otherwuse = n * fac ( n -1 )
--- week1.hs
+++ week1.hs
@@ -1,3 +1,14 @@
+circumferenceOfCircle :: Float -> Float
+circumferenceOfCircle d = pi * d
+
+sideOfCylinder :: Float -> Float -> Float
+sideOfCylinder d h = circumferenceOfCircle d * h
+
+canDrink :: Int -> Bool
+canDrink age = age >= 18
+
+all3CanDrink :: Int -> Int -> Int -> Bool
+all3CanDrink a b c = canDrink a && canDrink b && canDrink c
 
 timesTen :: Int -> Int
 timesTen num = num * 10
