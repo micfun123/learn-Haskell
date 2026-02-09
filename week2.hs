0ed5ee4a919db177c2cce2a46b6f7f9753eb87cd
Michael Parker <michaelrbparker@protonmail.com>
2026-02-09 22:44:30 +0000

    currected comments

--- week2.hs
+++ week2.hs
@@ -41,36 +41,37 @@ validDate day month
   | otherwise = True
 
 {-
-Evaluation and calculation exercises:
 
 1. sumThree 3 5 7
-   ⇝ 3 + 5 + 7
-   ⇝ 8 + 7
-   ⇝ 15
+   -> 3 + 5 + 7         def of sumThree
+   -> 8 + 7             arithmetic
+   -> 15                arithmetic
 
    sumThree 8 (1 + 3) 2
-   ⇝ 8 + (1 + 3) + 2
-   ⇝ 8 + 4 + 2
-   ⇝ 12 + 2
-   ⇝ 14
+   -> 8 + (1 + 3) + 2   def of sumThree
+   -> 8 + 4 + 2         arithmetic
+   -> 12 + 2            arithmetic
+   -> 14                arithmetic
 
 2. threeDifferent 1 4 2
-   ⇝ 1 /= 4 && 4 /= 2 && 1 /= 2
-   ⇝ True && True && True
-   ⇝ True
+   -> 1 /= 4 && 4 /= 2 && 1 /= 2    def of threeDifferent
+   -> True && True && True          arithmetic/logic
+   -> True                          arithmetic/logic
 
    threeDifferent 1 7 7
-   ⇝ 1 /= 7 && 7 /= 7 && 1 /= 7
-   ⇝ True && False && True
-   ⇝ False
+   -> 1 /= 7 && 7 /= 7 && 1 /= 7    def of threeDifferent
+   -> True && False && True         arithmetic/logic
+   -> False                         arithmetic/logic
 
 3. howManyEqual 3 5 2
-   ⇝ 3 == 5 && 5 == 2  -- False
-   ⇝ 3 == 5 || 5 == 2 || 3 == 2 -- False
-   ⇝ 0
+   -> 3 == 5 && 5 == 2              def of howManyEqual (guard 1)
+   -> 3 == 5 || 5 == 2 || 3 == 2    def of howManyEqual (guard 2)
+   -> False || False || False       arithmetic/logic
+   -> 0                             otherwise
 
    howManyEqual 5 2 5
-   ⇝ 5 == 2 && 2 == 5 -- False
-   ⇝ 5 == 2 || 2 == 5 || 5 == 5 -- True
-   ⇝ 2
+   -> 5 == 2 && 2 == 5              def of howManyEqual (guard 1)
+   -> 5 == 2 || 2 == 5 || 5 == 5    def of howManyEqual (guard 2)
+   -> False || False || True        arithmetic/logic
+   -> 2                             arithmetic/logic
 -}
