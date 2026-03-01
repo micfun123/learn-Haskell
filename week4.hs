bd97c5a0daea06e9cba4a50d142e0ec61667cc15
Michael Parker <michaelrbparker@protonmail.com>
2026-03-01 16:22:17 +0000

    fixed week 4

--- week4.hs
+++ week4.hs
@@ -21,14 +21,13 @@ capMark (name, mark)
 
 firstNumbers :: Int -> [Int]
 firstNumbers n
-    | n < 0     = error "Negative input not allowed"
-    | otherwise = [0..(n-1)]
-
+    | n <= 0    = []
+    | otherwise = [1..n]
 
 firstSquares :: Int -> [Int]
 firstSquares n
-    | n < 0     = error "Negative input not allowed"
-    | otherwise = [x^2 | x <- [0..(n-1)]]
+    | n <= 0    = []
+    | otherwise = [x^2 | x <- firstNumbers n]
 
 
 capitalise :: String -> String
@@ -48,19 +47,33 @@ duplicate str 0 = ""
 duplicate str n = str ++ duplicate str (n-1)
 
 
+{-
+
+duplicate :: String -> Int -> String
+duplicate str n = concat [str | _ <- [1..n]]
+
+-}
+
+
 divisors :: Int -> [Int]
 divisors n
-    | n <= 0    = error "Input must be a positive integer"
-    | otherwise = [x | x <- [1..n], n `mod` x == 0]
+    | n <= 0    = []
+    | otherwise = [x | x <- [1..n], mod n x == 0]
 
 isPrime :: Int -> Bool
 isPrime n
     | n <= 1    = False
     | otherwise = divisors n == [1, n]
 
+
+split :: [(a,b)] -> ([a],[b])
+split xs = ([fst p | p <- xs], [snd p | p <- xs])
+
+
+{-
 split:: [(a,b)] -> ([a],[b])
 split [] = ([], [])
 split ((x,y):xs) = (x:xs1, y:xs2)
     where (xs1, xs2) = split xs
-
+-}
     
