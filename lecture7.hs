c786874b3db6c6ddbcf10ead7acf34ed4760822a
Michael Parker <michaelrbparker@protonmail.com>
2026-03-16 10:51:09 +0000

    lecutre 7

--- lecture7.hs
+++ lecture7.hs
@@ -38,3 +38,18 @@ names' = map name
 
 data Shape = Circle Float | Rectangle Float Float
       deriving (Show,Eq)
+
+perimeter :: Shape -> Float
+perimeter (Circle r) = 2 * pi * r
+perometer (Rectangle h w) = 2 * ( h + w)
+
+data Tree = Null | Node Int Tree Tree
+      deriving(Eq,Show)
+      
+tree0 = Null
+tree1 = Node 3 Null Null 
+tree2 = Node 5 (Node 3 Null Null) (Node 7(Node 6 Null Null) Null)
+
+height :: Tree -> Int
+height Null = 0
+height (Node _ lnode rnode) = 1 + max (height lnode) ( height rnode)
